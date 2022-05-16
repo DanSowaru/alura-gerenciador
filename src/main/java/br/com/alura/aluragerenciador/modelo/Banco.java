f36527ad5f85b846ded88df3d9a17186c3ca88fa
Danilo Soares <prof.danilomotasoares@gmail.com>
2022-05-16 18:20:27 -0300

    made with terminal

--- src/main/java/br/com/alura/aluragerenciador/modelo/Banco.java
+++ src/main/java/br/com/alura/aluragerenciador/modelo/Banco.java
@@ -7,8 +7,9 @@ import java.util.List;
 public class Banco {
 	
 	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuario> listaUsuarios = new ArrayList<>();
 	private static Integer chaveSequencial = 1;
-	
+
 	static {
 		Empresa empresa = new Empresa();
 		empresa.setNome("Alura");
@@ -18,6 +19,15 @@ public class Banco {
 		empresa2.setId(chaveSequencial++);
 		lista.add(empresa);
 		lista.add(empresa2);
+
+		Usuario u1 = new Usuario();
+		u1.setLogin("nico");
+		u1.setSenha("12345");
+		Usuario u2 = new Usuario();
+		u1.setLogin("ana");
+		u1.setSenha("12345");
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
 	}
 
 	public void adiciona(Empresa empresa) {
@@ -54,6 +64,15 @@ public class Banco {
 		for (Empresa empresa : lista) {
 			if (empresa.getId().equals(id)) {
 				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		for (Usuario usuario : listaUsuarios) {
+			if (usuario.ehIgual(login, senha)) {
+				return usuario;
 			}
 		}
 		return null;
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -10,7 +10,7 @@
 <br/>
 <a href="hello-servlet">Hello Servlet (estamos no fast)</a>
 
-<a href="formNovaEmpresa.jsp">Nova Empresa</a>
+<a href="entrada?acao=NovaEmpresaForm">Nova Empresa</a>
 <a href="entrada?acao=ListaEmpresas">Lista de Empresas</a>
 </body>
 </html>
